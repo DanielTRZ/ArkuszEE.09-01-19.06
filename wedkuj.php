5d404c18797288572efb0bd5837d4cf95dba8aac
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-04-28 20:42:56 +0200

    Add files via upload

---
+++ wedkuj.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+       <meta charset="utf-8">
+        <title>Wędkujemy</title>
+        
+        <link rel="stylesheet" href="styl_1.css">
+        
+        
+    </head>
+    <body>
+        
+        <div class="baner">
+            <h1>Portal dla wędkarzy</h1>
+        </div>
+        <div class="lewy">
+            <h2>Ryby drapieżne naszych wód</h2>
+            <?php
+             $baza=mysqli_connect('localhost','root','','wedkowanie');
+            if(mysqli_connect_errno()){
+                echo "wystąpił błąd połączenia z bazą";
+            }
+            $wynik=mysqli_query($baza,"SELECT `nazwa`,`wystepowanie` FROM `ryby` WHERE `styl_zycia` = 1");
+            while($row=mysqli_fetch_array($wynik))
+            {
+                
+                
+            echo "<ul>";
+            echo "<li>";
+            echo  $row['nazwa'].","." występowanie: ".$row['wystepowanie'];"<br>";
+            echo "</ul>";
+            echo "</li>";
+            }
+            mysqli_close($baza);
+                
+            ?>
+        </div>
+        <div class="prawy">
+            <img src="ryba1.jpg" alt="sum"><br>
+            <a href="kweredny.txt">Pobierz kwerendy</a>
+            
+        </div>
+        <div class="stopka">
+            <p>Stronę wykonał: Daniel </p>
+            
+        </div>
+        
+        
+        
+        
+    </body>
+    
+    
+    
+    
+    
+</html>
